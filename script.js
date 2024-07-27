1e85e1863f72a727b5e5b04e44ea94dd0f650df1
Esteban-UXUI <esteban.ui.ux@gmail.com>
2024-07-27 17:05:13 -0500

    Se agrega menu

--- script.js
+++ script.js
@@ -4,4 +4,10 @@
     const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
     el.style.setProperty('--posX',  x-l-w/2);
     el.style.setProperty('--posY',  y-t-h/2);
+
+    navToggle.addEventListener('click', function() {
+        navList.classList.toggle('active');
+      });
+    
   })
+  
