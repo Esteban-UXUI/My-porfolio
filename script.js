ce3c4747a99d7afbffd538d280a81992b4866a65
Esteban-UXUI <esteban.ui.ux@gmail.com>
2024-07-27 17:51:25 -0500

    menu updatte

--- script.js
+++ script.js
@@ -7,15 +7,29 @@
 
   })
 
-  $(document).ready(function(){
 
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('header').addClass('header2');
-		} else {
-			$('header').removeClass('header2');
-		}
-	});
+  // JavaScript Version
+const buttonMenu = document.querySelector('#nav-mobile');
+const navMenu = document.querySelector('.nav-menu');
 
+buttonMenu.addEventListener('click', (e) => {
+  e.currentTarget.classList.toggle('nav-open');
+  navMenu.classList.toggle('open-menu');
 });
+
+// jQuery Version
+// $(function() {
+//     var btn_movil = $(‘#nav-mobile’),
+//     menu = $(‘#menu’).find(‘ul’);
+
+//     // Al dar click agregar/quitar clases que permiten el despliegue del menú
+//     btn_movil.on(‘click’, function (e) {
+//         e.preventDefault();
+//         var el = $(this);
+//         el.toggleClass(‘nav-active’);
+//         menu.toggleClass(‘open-menu’);
+//     });
+// });
+
+
   
