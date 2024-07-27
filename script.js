274318e6e0247abfd70501e8af219baacf6bf29a
Esteban-UXUI <esteban.ui.ux@gmail.com>
2024-07-27 17:29:44 -0500

    update

--- script.js
+++ script.js
@@ -4,17 +4,8 @@
     const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
     el.style.setProperty('--posX',  x-l-w/2);
     el.style.setProperty('--posY',  y-t-h/2);
-  }
- 
-    $(document).ready(function(){
 
-        $(window).scroll(function(){
-            if( $(this).scrollTop() > 0 ){
-                $('header').addClass('header2');
-            } else {
-                $('header').removeClass('header2');
-            }
-        });
-      
-  }
-)
+   
+    
+  })
+  
