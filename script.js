ac5b2275b7144696eb73080df2293b76eb43e644
Esteban-UXUI <esteban.ui.ux@gmail.com>
2024-07-27 23:05:38 -0500

    final navbar

--- script.js
+++ script.js
@@ -5,39 +5,40 @@
     el.style.setProperty('--posX',  x-l-w/2);
     el.style.setProperty('--posY',  y-t-h/2);
 
+    const navSlide = () => {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-links");
+        const navLinks = document.querySelectorAll(".nav-links li");
+      
+        // Toggle Nav
+        burger.addEventListener("click", () => {
+          nav.classList.toggle("nav-active");
+      
+          // Animate Links
+          navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+              link.style.animation = "";
+            } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+                0.5}s`;
+            }
+          });
+          // Burger Animation
+          burger.classList.toggle("toggle");
+        });
+      };
+      
+      const app = () => {
+        navSlide();
+      };
+      
+      app();
+
   })
 
-  document.querySelector('.menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.nav-links').classList.toggle('active');
-});
+  
 
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
-  
-    // Toggle Nav
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-  
-      // Animate Links
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-            0.5}s`;
-        }
-      });
-      // Burger Animation
-      burger.classList.toggle("toggle");
-    });
-  };
-  
-  const app = () => {
-    navSlide();
-  };
-  
-  app();
+
+
+
   
